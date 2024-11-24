74db4a9b39b1b7a5b9225ec8d42ab1cb0ec11000
AshKemp <kmuraliashwin@gmail.com>
2024-11-24 09:05:35 +0530

    define app routes using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import { useState } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layout/app-layout";
+import LandingPage from "./pages/landing";
+import OnboardingPage from "./pages/onboarding";
+import JobListing from "./pages/job-listing";
+import JobPage from "./pages/job";
+import PostJob from "./pages/post-job";
+import SavedJobs from "./pages/saved-jobs";
+import MyJobs from "./pages/my-jobs";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <OnboardingPage />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-job",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <Button>Login</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
